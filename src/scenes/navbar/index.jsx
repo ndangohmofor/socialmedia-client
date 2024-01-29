79e0fb5ce474cf4ebbf2d27d254beeee43c67845
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-28 22:09:56 -0500

    start building the navbar

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMoke,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+
+import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "../../state";
+import FlexBetween from "../../components/FlexBetween";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Nav Bar</div>;
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNoneMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
+  return (
+    <FlexBetween padding={"1rem 6%"} background={alt}>
+      <FlexBetween gap={"1.75rem"}></FlexBetween>
+    </FlexBetween>
+  );
 };
 
 export default Navbar;
