8295787d0127e050992dea11f52fc90c0fe063dc
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-29 23:12:46 -0500

    Fix the background color of the navbar

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
   const fullName = "Ivo Mofor"; /* `${user.firstName} ${user.lastName}`; */
 
   return (
-    <FlexBetween padding={"1rem 6%"} background={alt}>
+    <FlexBetween padding={"1rem 6%"} backgroundColor={alt}>
       <FlexBetween gap={"1.75rem"}>
         <Typography
           fontWeight={"bold"}
