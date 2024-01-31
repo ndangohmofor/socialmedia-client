128af652e0a801b240b2bfb3fba1891413abeaf2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-31 00:14:51 -0500

    Create input for the form location

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -112,6 +112,19 @@ const Form = () => {
                     gridColumn: "span 2",
                   }}
                 />
+
+                <TextField
+                  label={"Location"}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && errors.location}
+                  sx={{
+                    gridColumn: "span 2",
+                  }}
+                />
               </>
             )}
           </Box>
