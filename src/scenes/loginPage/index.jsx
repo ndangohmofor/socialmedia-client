498dc99c28dc7b51a7f096579618fe1e462c37f5
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-30 23:25:43 -0500

    Add the heading/navbar on login page

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -7,7 +7,20 @@ const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("( min-width: 1000px)");
 
-  return <Box></Box>;
+  return (
+    <Box>
+      <Box
+        width={"100%"}
+        backgroundColor={theme.palette.background.alt}
+        p={"1rem 6%"}
+        textAlign={"center"}
+      >
+        <Typography fontWeight={"bold"} fontSize={"32px"} color={"primary"}>
+          SWACO
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
