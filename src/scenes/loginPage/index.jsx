f2ca84ca4ab6bc42ff71448bbeb95c7b6f5d251c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-30 23:29:58 -0500

    Add the box for the login form

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -19,6 +19,17 @@ const LoginPage = () => {
           SWACO
         </Typography>
       </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius={"1.5rem"}
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight={"500"} variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SWACO - Social Media for Southern Cameroonians In Ohio
+        </Typography>
+      </Box>
     </Box>
   );
 };
