ad024ed6711464e9c58e07e4f0e1f8cddde208cb
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-30 23:09:50 -0500

    Starting the login page

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useTheme, useMediaQuery } from "@mui/material";
 
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("( min-width: 1000px)");
+
+  return <Box></Box>;
 };
 
 export default LoginPage;
