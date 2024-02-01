7455d1c0bf1ee95dd45b6606e9ff3c9d708a026c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-01 06:42:50 -0500

    Fix the closing tag of the box surrounding the dropzone

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -147,36 +147,36 @@ const Form = () => {
                   border={`1px solid ${palette.neutral.medium}`}
                   borderRadius={"5px"}
                   p={"1rem"}
-                />
-
-                <Dropzone
-                  acceptedFiles=".jpg, .jpeg, .png"
-                  multiple={false}
-                  onDrop={(acceptedFiles) =>
-                    setFieldValue("picture", acceptedFiles[0])
-                  }
                 >
-                  {({ getRootProps, getInputProps }) => (
-                    <Box
-                      {...getRootProps()}
-                      border={`2px dashed ${palette.primary.main}`}
-                      p={"1rem"}
-                      sx={{
-                        "&:hover": { cursor: "pointer" },
-                      }}
-                    >
-                      <input {...getInputProps()} />
-                      {!values.picutre ? (
-                        <p>Add Picture Here</p>
-                      ) : (
-                        <FlexBetween>
-                          <Typography>{values.picture.name}</Typography>
-                          <EditOutlinedIcon />
-                        </FlexBetween>
-                      )}
-                    </Box>
-                  )}
-                </Dropzone>
+                  <Dropzone
+                    acceptedFiles=".jpg, .jpeg, .png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p={"1rem"}
+                        sx={{
+                          "&:hover": { cursor: "pointer" },
+                        }}
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picutre ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
+                </Box>
               </>
             )}
           </Box>
