ae3d84471268b96676abbb1250e19767e9c93deb
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-01 06:35:35 -0500

    Add the Dropzone for uploading a picture

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -148,6 +148,35 @@ const Form = () => {
                   borderRadius={"5px"}
                   p={"1rem"}
                 />
+
+                <Dropzone
+                  acceptedFiles=".jpg, .jpeg, .png"
+                  multiple={false}
+                  onDrop={(acceptedFiles) =>
+                    setFieldValue("picture", acceptedFiles[0])
+                  }
+                >
+                  {({ getRootProps, getInputProps }) => (
+                    <Box
+                      {...getRootProps()}
+                      border={`2px dashed ${palette.primary.main}`}
+                      p={"1rem"}
+                      sx={{
+                        "&:hover": { cursor: "pointer" },
+                      }}
+                    >
+                      <input {...getInputProps()} />
+                      {!values.picutre ? (
+                        <p>Add Picture Here</p>
+                      ) : (
+                        <FlexBetween>
+                          <Typography>{values.picture.name}</Typography>
+                          <EditOutlinedIcon />
+                        </FlexBetween>
+                      )}
+                    </Box>
+                  )}
+                </Dropzone>
               </>
             )}
           </Box>
