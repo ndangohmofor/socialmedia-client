786275fa64139e6cbbc3b8bb58582d3b214c0f67
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-03 21:53:21 -0500

    Add the linkedin social profile section in the fourth row

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -136,6 +136,20 @@ const UserWidget = ({ userId, picturePath }) => {
                 <Typography color={medium}>Social Network</Typography>
               </Box>
             </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+
+          <FlexBetween gap={"1rem"}>
+            <FlexBetween gap={"1rem"}>
+              <img src="../assets/linkedin.png" alt="linkedin" />
+              <Box>
+                <Typography color={main} fontWeight={"500"}>
+                  LinkedIn
+                </Typography>
+                <Typography color={medium}>Network Platform</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
           </FlexBetween>
         </Box>
       </FlexBetween>
