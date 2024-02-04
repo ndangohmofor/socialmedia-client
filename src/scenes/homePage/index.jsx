28adcdaa456f91f8c239ae3eb6b6a01610d37121
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-03 21:55:35 -0500

    Add the mobile screen variable and extract user details from state

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,8 +1,12 @@
 import Box from "@mui/material/Box";
 import React from "react";
 import Navbar from "../navbar";
+import { useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <Navbar />
