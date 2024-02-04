3922007f59c63d91bf7decc85a5c6db3047d7dbd
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-03 21:25:16 -0500

    Add the occupation section in the second row

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -94,6 +94,10 @@ const UserWidget = ({ userId, picturePath }) => {
             <LocationOnOutlined fontSize="large" sx={{ color: main }} />
             <Typography color={medium}>{location}</Typography>
           </Box>
+          <Box display={"flex"} alignItems={"center"} gap={"1rem"}>
+            <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{occupation}</Typography>
+          </Box>
         </Box>
       </FlexBetween>
     </WidgetWrapper>
