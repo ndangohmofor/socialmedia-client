77a1beba5938f22a9bd0e834d7a42a816739a5fa
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-04 20:35:42 -0500

    fix count of number of friends to display on the user widget

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -77,7 +77,9 @@ const UserWidget = ({ userId, picturePath }) => {
             >
               {firstName} {lastName}
             </Typography>
-            <Typography color={medium}>{friends.length} friends</Typography>
+            <Typography color={medium}>
+              {friends ? friends.length : 0} friends
+            </Typography>
           </Box>
         </FlexBetween>
         <ManageAccountsOutlined />
