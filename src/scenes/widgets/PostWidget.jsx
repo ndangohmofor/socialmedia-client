94e7927381a6c3249b2b4b6b116727a99dd0e647
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-06 21:26:43 -0500

    Add required imports into the Post Widget

---
+++ src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,18 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+
+const PostWidget = () => {
+  return <div></div>;
+};
+
+export default PostWidget;
