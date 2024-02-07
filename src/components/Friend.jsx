82decf7dcc185bd7e2e7e2d9b0406c6186f20c7e
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-06 21:52:12 -0500

    Add icons for adding and removing friends from friends list

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -39,7 +39,57 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     dispatch(setFriends({ friends: data }));
   };
 
-  return <div></div>;
+  return (
+    <FlexBetween>
+      <FlexBetween gap={"1rem"}>
+        <UserImage image={userPicturePath} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight={"500"}
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography color={main} fontSize={"0.75rem"}>
+            {subtitle}
+          </Typography>
+        </Box>
+      </FlexBetween>
+      <IconButton
+        onClick={() => patchFriend()}
+        sx={{
+          backgroundColor: primaryLight,
+          p: "0.6rem",
+        }}
+      >
+        {isFriend ? (
+          <PersonRemoveOutlined
+            sx={{
+              color: primaryDark,
+            }}
+          />
+        ) : (
+          <PersonAddOutlined
+            sx={{
+              color: primaryDark,
+            }}
+          />
+        )}
+      </IconButton>
+    </FlexBetween>
+  );
 };
 
 export default Friend;
