7b007c13daac5f17a2800dc173c676f69228150b
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-06 21:20:44 -0500

    Return a post widget for each post

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -33,6 +33,38 @@ const PostsWidget = ({ useId, isProfile = false }) => {
       getPosts();
     }
   }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </>
+  );
 };
 
 export default PostsWidget;
