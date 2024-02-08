0f5c5786ddb8cc62fbe3301886829afde6a3b9ec
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-07 22:36:15 -0500

    Fix typo in url causing calls to backend to fail

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -39,7 +39,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`http://locahost:3001/posts/${postId}/like`, {
+    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
