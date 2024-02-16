8111fea5f311f1cc55d1bdb236b855b5fc44004a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-02-16 06:12:34 -0500

    Reverting axios function temporarily

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -8,7 +8,6 @@ import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const dispatch = useDispatch();
@@ -26,9 +25,10 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const isFriend = friends.find((friend) => friend._id === friendId);
 
   const patchFriend = async () => {
-    const response = await axios.patch(
+    const response = await fetch(
       `http://localhost:3001/users/${_id}/${friendId}`,
       {
+        method: "PATCH",
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
